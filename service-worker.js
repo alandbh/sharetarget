6671d92405fd935a6218a5b58e3be779078a24df
Alan Vasconcelos <alandbh@gmail.com>
2024-09-15 21:58:11 -0300

    trying to get the imagen on index

--- service-worker.js
+++ service-worker.js
@@ -8,33 +8,84 @@ self.addEventListener("activate", (event) => {
     return self.clients.claim();
 });
 
-self.addEventListener("fetch", async function (event) {
-    console.log("👷", "fetch", event);
-    const url = new URL(event.request.url);
-    // If this is an incoming POST request for the
-    // registered "action" URL, respond to it.
-    if (event.request.method === "POST" && url.pathname === "/target.html") {
-        // const formData = await event.request.formData();
-        // const file = formData.get("file");
+// self.addEventListener("fetch", async function (event) {
+//     console.log("👷", "fetch", event);
+//     const url = new URL(event.request.url);
+//     // If this is an incoming POST request for the
+//     // registered "action" URL, respond to it.
+//     if (event.request.method === "POST" && url.pathname === "/target.html") {
+//         // const formData = await event.request.formData();
+//         // const file = formData.get("file");
 
-        // const fileReader = new FileReader();
-        // fileReader.readAsDataURL(file);
-        // fileReader.addEventListener("load", (event) => {
-        //     const base64Url = fileReader.result;
+//         // const fileReader = new FileReader();
+//         // fileReader.readAsDataURL(file);
+//         // fileReader.addEventListener("load", (event) => {
+//         //     const base64Url = fileReader.result;
 
-        //     const img = new Image();
-        //     img.src = base64Url;
-        //     document.body.appendChild(img);
-        // });
+//         //     const img = new Image();
+//         //     img.src = base64Url;
+//         //     document.body.appendChild(img);
+//         // });
 
+//         event.respondWith(
+//             (async () => {
+//                 const formData = await event.request.formData();
+//                 const file = formData.get("file");
+//                 const filename = file.name.replaceAll(" ", "_");
+
+//                 if (file) {
+//                     // Envia o arquivo para a página Target
+//                     self.clients.matchAll().then((clients) => {
+//                         clients.forEach((client) => {
+//                             if (client.url.endsWith("target.html")) {
+//                                 client.postMessage({
+//                                     type: "file",
+//                                     file: {
+//                                         name: file.name,
+//                                         type: file.type,
+//                                         blob: file,
+//                                     },
+//                                 });
+//                             }
+//                         });
+//                     });
+//                 }
+
+//                 // Retorna uma resposta padrão
+//                 // return new Response(
+//                 //     JSON.stringify({ status: "file received" }),
+//                 //     { headers: { "Content-Type": "application/json" } }
+//                 // );
+//                 const responseUrl = "/target.html?filename=" + filename;
+//                 return Response.redirect(responseUrl, 303);
+
+//                 // const fileReader = new FileReader();
+//                 // fileReader.readAsDataURL(file);
+//                 // fileReader.addEventListener("load", (event) => {
+//                 //     const base64Url = fileReader.result;
+//                 //     const img = new Image();
+//                 //     img.src = base64Url;
+//                 //     document.body.appendChild(img);
+//                 // });
+//                 // const filename = file.name.replaceAll(" ", "_");
+//                 // const link = formData.get("link") || "";
+//                 // const responseUrl = "/target.html?filename=" + filename;
+//                 // return Response.redirect(responseUrl, 303);
+//             })()
+//         );
+//     }
+// });
+
+// service-worker.js
+self.addEventListener("fetch", (event) => {
+    if (event.request.method === "POST") {
         event.respondWith(
             (async () => {
                 const formData = await event.request.formData();
                 const file = formData.get("file");
-                const filename = file.name.replaceAll(" ", "_");
 
                 if (file) {
-                    // Envia o arquivo para a página Target
+                    // Envia o arquivo de imagem para a página
                     self.clients.matchAll().then((clients) => {
                         clients.forEach((client) => {
                             if (client.url.endsWith("target.html")) {
@@ -51,26 +102,8 @@ self.addEventListener("fetch", async function (event) {
                     });
                 }
 
-                // Retorna uma resposta padrão
-                // return new Response(
-                //     JSON.stringify({ status: "file received" }),
-                //     { headers: { "Content-Type": "application/json" } }
-                // );
-                const responseUrl = "/target.html?filename=" + filename;
-                return Response.redirect(responseUrl, 303);
-
-                // const fileReader = new FileReader();
-                // fileReader.readAsDataURL(file);
-                // fileReader.addEventListener("load", (event) => {
-                //     const base64Url = fileReader.result;
-                //     const img = new Image();
-                //     img.src = base64Url;
-                //     document.body.appendChild(img);
-                // });
-                // const filename = file.name.replaceAll(" ", "_");
-                // const link = formData.get("link") || "";
-                // const responseUrl = "/target.html?filename=" + filename;
-                // return Response.redirect(responseUrl, 303);
+                // Redireciona para a página target.html
+                return Response.redirect("/target.html", 303); // 303: See Other
             })()
         );
     }
