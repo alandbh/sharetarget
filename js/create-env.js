859eb40bc2eb341a1fb5a08a4f8791874746a064
Alan Vasconcelos <alandbh@gmail.com>
2024-09-17 18:12:17 -0300

    adding netlify functions

--- js/create-env.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-fs.writeFileSync(
-    "./js/config.js",
-    `
-    window.API_KEY="${process.env.CLIENT_ID}";\n
-    window.API_KEY="${process.env.API_KEY}";\n
-`
-);
---
+++ netlify/functions/index.mjs
@@ -0,0 +1,10 @@
+export default async (req, context) => {
+    const fs = require("fs");
+    fs.writeFileSync(
+        "./js/config.js",
+        `
+        window.API_KEY="${process.env.CLIENT_ID}";\n
+        window.API_KEY="${process.env.API_KEY}";\n
+    `
+    );
+};
